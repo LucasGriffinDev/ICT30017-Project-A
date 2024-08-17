31131de89030b7968dbadf2cf5bb1db554e465e3
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2024-08-17 20:21:02 +1000

    compnents

---
+++ src/app/components/heroComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroComponent = () => {
+  return (
+    <>
+      <h1>hero compnent</h1>
+    </>
+  );
+};
+
+export default HeroComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
+import Image from 'next/image';
+import HeroComponent from './components/heroComponent';
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-500 p-4">
-      <h1 className="text-4xl font-bold mb-6">Home</h1>
+      <h1 className="text-4xl font-bold mb-6">Aged Care managment app</h1>
 
-      <div className="mt-4 p-4 bg-white shadow rounded-lg text-black">
-        Landing page
-      </div>
+      <HeroComponent />
       <div className="mt-4 p-4 bg-white shadow rounded-lg text-black">
         Hot Reloading
       </div>
-      
     </div>
   );
 }
