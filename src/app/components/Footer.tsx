82b0ce5a4580b2bb1e3cee285800b74607e90c00
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2024-08-24 19:50:20 +1000

    Create Footer.tsx

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+  return (
+    <>
+      <footer className="w-full flex justify-center items-center mt-12 p-4 bg-blue-900 text-white">
+        © 2024 Aged Care Management Inc. All rights reserved.
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
