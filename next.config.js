1d76859a3a730c4704fb649cbf93b10322e1325c
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2024-09-29 19:59:51 +1000

    pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/ICT30017-Project-A',
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
+    output: 'export',
+    basePath: '/ICT30017-Project-A',
+    trailingSlash: true,
+  };
+  
+  module.exports = nextConfig;
+  
